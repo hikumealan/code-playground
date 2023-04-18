fe026754aa1f08b4a383d20809143667b5dbbc80
Bryan Olson <hikumealan@hotmail.com>
2023-04-18 12:44:14 -0400

    Update 2FA.js

--- src/express/2FA.js
+++ src/express/2FA.js
@@ -61,3 +61,254 @@ app.listen(3000, () => {
 });
 
 // This code will create an Express app that enables 2FA for users. When a user logs in, they will be prompted to enter a 2FA code. If the code is valid, the user will be authenticated and redirected to the home page. If the code is invalid, the user will be redirected to the 2FA login page.
+
+// #######################################################################################################################
+const express = require('express');
+const app = express();
+
+// Create a database to store user accounts and 2FA tokens.
+const db = require('./db');
+
+// Create a function to generate a random 2FA token.
+const generateToken = () => {
+  return Math.random().toString(36).substring(7);
+};
+
+// Create a function to verify a 2FA token.
+const verifyToken = (token, user) => {
+  // Get the user's 2FA token from the database.
+  const storedToken = db.get2FAToken(user);
+
+  // If the token is not found, return false.
+  if (!storedToken) {
+    return false;
+  }
+
+  // Compare the token to the stored token.
+  return token === storedToken;
+};
+
+// Create a route to generate a new 2FA token for a user.
+app.post('/2fa/generate', (req, res) => {
+  // Get the user's ID from the request body.
+  const userId = req.body.userId;
+
+  // Generate a new 2FA token for the user.
+  const token = generateToken();
+
+  // Save the token to the database.
+  db.set2FAToken(userId, token);
+
+  // Send the token to the user.
+  res.send(token);
+});
+
+// Create a route to verify a 2FA token for a user.
+app.post('/2fa/verify', (req, res) => {
+  // Get the user's ID and token from the request body.
+  const userId = req.body.userId;
+  const token = req.body.token;
+
+  // Verify the token.
+  const isVerified = verifyToken(token, userId);
+
+  // If the token is not verified, return an error.
+  if (!isVerified) {
+    res.send('Invalid 2FA token.');
+    return;
+  }
+
+  // Authenticate the user.
+  res.send('Authenticated');
+});
+
+// Start the server.
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
+
+// This code will create an Express app that implements a vanilla 2FA. When a user logs in, they will be prompted to enter a 2FA token. If the token is valid, the user will be authenticated and logged in. If the token is invalid, the user will be prompted to enter the token again.
+
+
+// #######################################################################################################################
+
+const express = require('express');
+const app = express();
+
+// Create a secret key.
+const secret = 'my-secret';
+
+// Create a function to generate a 2FA code.
+const generateCode = () => {
+  // Generate a random number between 100000 and 999999.
+  const code = Math.floor(Math.random() * 900000) + 100000;
+
+  // Return the code.
+  return code;
+};
+
+// Create a function to verify a 2FA code.
+const verifyCode = (code, user) => {
+  // Get the user's 2FA secret.
+  const secret = user.twofa_secret;
+
+  // Check if the code is valid.
+  if (!validCode(code, secret)) {
+    return false;
+  }
+
+  // The code is valid.
+  return true;
+};
+
+// Create a function to send a 2FA code to a user.
+const sendCode = (user, otp) => {
+  // Get the user's email address.
+  const email = user.email;
+
+  // Send the code to the user's email address.
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'your@email.com',
+      pass: 'your-password',
+    },
+  });
+
+  // Create a mail message.
+  const mail = transporter.createMail({
+    from: 'your@email.com',
+    to: email,
+    subject: '2FA Code',
+    body: `Your 2FA code is: ${otp}`,
+  });
+
+  // Send the mail message.
+  transporter.sendMail(mail);
+};
+
+// Create a route to generate a 2FA code.
+app.get('/2fa/generate', (req, res) => {
+  // Generate a 2FA code.
+  const code = generateCode();
+
+  // Send the code to the user.
+  sendCode(req.user, code);
+
+  // Redirect the user to the 2FA login page.
+  res.redirect('/2fa/login');
+});
+
+// Create a route to verify a 2FA code.
+app.post('/2fa/login', (req, res) => {
+  // Get the 2FA code from the request body.
+  const code = req.body.code;
+
+  // Verify the 2FA code.
+  if (!verifyCode(code, req.user)) {
+    res.send('Invalid 2FA code.');
+    return;
+  }
+
+  // The code is valid.
+  res.redirect('/home');
+});
+
+// Start the server.
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
+
+// This code will create an Express app that enables 2FA for users. When a user requests a 2FA code, the app will generate a random code and send it to the user's email address. When the user logs in, they will be prompted to enter the 2FA code that was sent to them. If the code is valid, the user will be authenticated and redirected to the home page. If the code is invalid, the user will be redirected to the 2FA login page.
+
+// #######################################################################################################################
+
+const express = require('express');
+const app = express();
+
+// Set up the database.
+const db = require('./database');
+
+// Create a user model.
+const User = require('./models/user');
+
+// Create a 2FA middleware.
+const twoFactorMiddleware = (req, res, next) => {
+  // Check if the user is authenticated.
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+
+  // Get the user from the request.
+  const user = req.user;
+
+  // Check if the user has 2FA enabled.
+  if (!user.twoFactorEnabled) {
+    return next();
+  }
+
+  // Get the 2FA code from the request body.
+  const code = req.body.code;
+
+  // Validate the 2FA code.
+  if (!User.validateTwoFactorCode(user.id, code)) {
+    return res.send('Invalid 2FA code.');
+  }
+
+  // Next middleware.
+  next();
+};
+
+// Add the 2FA middleware to the application.
+app.use(twoFactorMiddleware);
+
+// Create a route to enable 2FA for a user.
+app.post('/users/:id/enable-two-factor', async (req, res) => {
+  // Get the user from the request.
+  const user = await User.findById(req.params.id);
+
+  // Enable 2FA for the user.
+  await user.enableTwoFactor();
+
+  // Send a success response.
+  res.send('Two-factor authentication enabled successfully.');
+});
+
+// Create a route to disable 2FA for a user.
+app.post('/users/:id/disable-two-factor', async (req, res) => {
+  // Get the user from the request.
+  const user = await User.findById(req.params.id);
+
+  // Disable 2FA for the user.
+  await user.disableTwoFactor();
+
+  // Send a success response.
+  res.send('Two-factor authentication disabled successfully.');
+});
+
+// Create a route to generate a new 2FA code for a user.
+app.post('/users/:id/generate-two-factor-code', async (req, res) => {
+  // Get the user from the request.
+  const user = await User.findById(req.params.id);
+
+  // Generate a new 2FA code for the user.
+  await user.generateTwoFactorCode();
+
+  // Send a success response.
+  res.send('A new 2FA code has been generated.');
+});
+
+// Start the server.
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
+
+/*
+This code will create an Express app that enables 2FA for users. The 2FA features are implemented using the following steps:
+
+The user is authenticated.
+The user's 2FA status is checked.
+If the user has 2FA enabled, the 2FA code is validated.
+If the 2FA code is valid, the user is authenticated.
+If the 2FA code is invalid, the user is denied access.
+*/
